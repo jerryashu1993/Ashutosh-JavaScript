2f86b5c42f534f0ef40bcdb0386a9d862f820d6d
jerryashu1993 <157955922+jerryashu1993@users.noreply.github.com>
2024-08-16 15:17:55 +0000

    container config done

---
+++ javabasic/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Ashutosh Das")
+
+let name = "Ashutosh Das"
+let age = 29
+let isLoggedIn = false
+let state;
+
+
+// (number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique) = This all are premitive data types.
+
+
+// object
+console.log(typeof "Ashutosh Das") // It's type is "String"
+console.log(typeof age) // It's type is "Number"
+console.log(typeof null) // It's type is "object"
+console.log(typeof Undefined) // It's type is "Undefined". It is only a type.
+
--- javabasic/variables.js
+++ javabasic/variables.js
@@ -1,8 +1,8 @@
 const AccountId = 144553
 let AccountEmail1 = "ashutosh@gmail.com"
-var AccountPassword1 = "12345"
+let AccountPassword1 = "12345"
 let AccountCity1 = "Udaipur"
-let AccountState1 = "Rajasthan"
+let AccountState1;
 
 // AccountId = 2 // not execute
 
@@ -12,14 +12,14 @@ AccountCity = "Jaipur"
 AccountState = "Haryana"
 
 console.log(AccountId)
-console.log(AccountEmail)
-console.log(AccountPassword)
-console.log(AccountCity)
-console.log(AccountState)
+console.log(AccountEmail1)
+console.log(AccountPassword1)
+console.log(AccountCity1)
+console.log(AccountState1)
 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope 
 */
 
-console.table([AccountId, AccountEmail1, AccountPassword1, AccountCity1, AccountState1])
+console.table([AccountId, AccountEmail, AccountPassword, AccountCity, AccountState])
